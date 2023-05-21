48e468586e6ff72a262d3bece0a539b84aa287fa
Elena Sharipova <elena.sh.r@yandex.ru>
2023-05-22 00:30:58 +0300

    Initial Commit

---
+++ CoffeeCompass/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoffeeCompass
+//
+//  Created by Elena Sharipova on 22.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
