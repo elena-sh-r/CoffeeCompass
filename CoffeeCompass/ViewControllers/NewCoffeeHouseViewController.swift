bcb1668080e7b994c2ef73bb2f374ee42aedb527
Elena Sharipova <elena.sh.r@yandex.ru>
2023-05-23 17:37:51 +0300

    feat: design for NewCoffeeHouseViewController implemented

---
+++ CoffeeCompass/ViewControllers/NewCoffeeHouseViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NewCoffeeHouseViewController.swift
+//  CoffeeCompass
+//
+//  Created by Elena Sharipova on 23.05.2023.
+//
+
+import UIKit
+
+class NewCoffeeHouseViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+    }
+}
+
+// MARK: - Table view delegate
+extension NewCoffeeHouseViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+}
+
+// MARK: - Text field delegate
+extension NewCoffeeHouseViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
