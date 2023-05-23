8f70212304e75faa2a1354652a6db1bad43a6432
Elena Sharipova <elena.sh.r@yandex.ru>
2023-05-23 20:09:50 +0300

    feat: image picker controller implemented

--- CoffeeCompass/ViewControllers/NewCoffeeHouseViewController.swift
+++ CoffeeCompass/ViewControllers/NewCoffeeHouseViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class NewCoffeeHouseViewController: UITableViewController {
+final class NewCoffeeHouseViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView()
@@ -18,7 +18,27 @@ class NewCoffeeHouseViewController: UITableViewController {
 extension NewCoffeeHouseViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let actionSheet = UIAlertController(
+                title: nil,
+                message: nil,
+                preferredStyle: .actionSheet
+            )
             
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
         } else {
             view.endEditing(true)
         }
@@ -30,5 +50,17 @@ extension NewCoffeeHouseViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+// MARK: - Work with image
+extension NewCoffeeHouseViewController {
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
     }
 }
